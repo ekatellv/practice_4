718f7e21c4e2d5e1eeb9ead98e3ce1cff9a143ef
ekatellv <159680448+ekatellv@users.noreply.github.com>
2024-06-21 12:34:52 +0700

    Create ex_2.py

---
+++ ex_2.py
@@ -0,0 +1,8 @@
+print('Какие два слова передал первой радиограммой Александр Попов?')
+first_word = input('Первое слово: ')
+second_word = input('Второе слово: ')
+if first_word != 'ясный' and second_word != 'сокол':
+    print('Неверно')
+else:
+    print('Верно')
+
