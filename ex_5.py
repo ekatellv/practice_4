f716ec04357ee41493296f24ebd584d0fa0dfead
ekatellv <159680448+ekatellv@users.noreply.github.com>
2024-06-21 13:12:01 +0700

    Create ex_5.py

---
+++ ex_5.py
@@ -0,0 +1,6 @@
+fishman_1 = int(input('Введите, сколько поймал первый рыбак: '))
+fishman_2 = int(input('Введите, сколько поймал второй рыбак: '))
+if fishman_1 < fishman_2:
+    print(fishman_1)
+else:
+    print(fishman_2)
